79625155f3f99b68e662cae6d5303db18401c02a
dostalm.04 <dostalm.04@spst.eu>
2022-10-19 07:59:07 +0200

    Přidejte soubory projektu.

---
+++ JedDatTyp07/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JedDatTyp07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int pP = 0;
+            int pC = 0;
+            int pOL = 0;
+            int pZ = 0;
+
+            for(int i = 0; i < textBox1.Lines.Count(); i++)
+            {
+                char znak = char.Parse(textBox1.Lines[i].ToString());
+                if(znak == '\\' )
+                {
+                    pOL++;
+                }
+
+
+            }
+
+
+
+
+        }
+    }
+}
